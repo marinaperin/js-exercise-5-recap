e34488d28502e7b708a4a9dd1eebbb6ae3329e50
Marina Perin <maryfrancisco@live.com>
2023-10-29 14:07:14 +0100

    feat | exercise 2

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Exercise 1
-
+/*
 function wordReverser (wordToReverse){
     let reversedWord = '';
     for (let i = wordToReverse.length -1; i >= 0; i--){
@@ -14,4 +14,31 @@ window.addEventListener('load', function(){
     let resultSpace = document.getElementById('result');
     resultSpace.innerText = `The reversed word is ${reversedUserWord}`;
 });
+*/
+// Exercise 2
+
+function stringChain (array, numberOfStrings){
+    let chain = '';
+    for (let i = 0; i < array.length; i++){
+        chain += array[i];
+        if (i+1 === numberOfStrings){
+            break;
+        }
+    }
+    return chain;
+}
+
+window.addEventListener('load', function(){
+    let stringArray = ['Hello,', ' today is', ' a good', ' day.', ' How are you?', ' Do you like M-', ' Mysteries?', ' Do you want to', ' play a game with me?'];
+    let concatenatedStrings = '';
+    const stringNumber = Number(prompt('How many strings do you want to concatenate?'));
+    /* while (0 > stringNumber || stringNumber > stringArray.length || isNaN(stringNumber)){
+        alert('Pick another number');
+        prompt('How many strings do you want to concatenate?');
+    }*/
+    concatenatedStrings = stringChain (stringArray, stringNumber);
+    let resultSpace = document.getElementById('result');
+    resultSpace.innerText = `The concatenated string is '${concatenatedStrings}'`;
+});
+
 
